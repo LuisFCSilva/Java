f0a42a55d00e9bfa3d6fa33b1890b4197f234a4b
Luis Fernando <lokluis@gmail.com>
2018-07-12 17:38:13 -0300

    Segundo Commit

--- Fundamentos/FirstApplication/src/Application.java
+++ Fundamentos/FirstApplication/src/Application.java
@@ -3,7 +3,9 @@ public class Application {
 
 	public static void main (String[] args) {
 		
-		System.out.println("Hello World");
+		var frase = "Hello World";
+		
+		System.out.println(frase);
 	}
 	
 }
